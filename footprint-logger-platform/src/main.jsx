b86e4ab05c6cb39c5cf78f5099c1f7f12bcde31c
Siya <107757390+Siya-S10-ai@users.noreply.github.com>
2026-04-09 12:54:49 +0200

    Update main.jsx

--- footprint-logger-platform/src/main.jsx
+++ footprint-logger-platform/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 import { store } from './store/store'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
